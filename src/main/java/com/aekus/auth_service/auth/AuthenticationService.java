56243d3b9dffa4b6fd3c0e84462a5cfe03ca6e5c
Aekus Trehan <aekus.trehan@gmail.com>
2024-08-14 23:55:44 -0400

    Created logic for token and activation code creation

--- src/main/java/com/aekus/auth_service/auth/AuthenticationService.java
+++ src/main/java/com/aekus/auth_service/auth/AuthenticationService.java
@@ -1,12 +1,16 @@
 package com.aekus.auth_service.auth;
 
 import com.aekus.auth_service.role.RoleRepository;
+import com.aekus.auth_service.user.Token;
+import com.aekus.auth_service.user.TokenRepository;
 import com.aekus.auth_service.user.User;
 import com.aekus.auth_service.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,6 +20,7 @@ public class AuthenticationService {
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
     public void register(RegistrationRequest request) {
         var userRole = roleRepository.findRoleByName("USER")
@@ -33,6 +38,38 @@ public class AuthenticationService {
                 .build();
 
         userRepository.save(user);
-        // todo: add sendValidationEmail
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        // todo: Add logic to send email
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // generate the token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(5))
+                .user(user)
+                .build();
+
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+
+        return codeBuilder.toString();
     }
 }
