e53c25f68172aa5f9bc6fd5bb68a8f41bbabb7e7
Aekus Trehan <aekus.trehan@gmail.com>
2024-08-14 23:49:07 -0400

    Added main logic for registering user

--- src/main/java/com/aekus/auth_service/auth/AuthenticationService.java
+++ src/main/java/com/aekus/auth_service/auth/AuthenticationService.java
@@ -1,11 +1,38 @@
 package com.aekus.auth_service.auth;
 
+import com.aekus.auth_service.role.RoleRepository;
+import com.aekus.auth_service.user.User;
+import com.aekus.auth_service.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
-    public void register(RegistrationRequest request) {
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findRoleByName("USER")
+                // todo: add better exception handling
+                .orElseThrow(() -> new IllegalStateException("Role USER was not initialized"));
+
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+
+        userRepository.save(user);
+        // todo: add sendValidationEmail
     }
 }
